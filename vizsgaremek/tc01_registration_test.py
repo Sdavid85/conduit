bc69f5fc697e29d16d6eebf5b450a6942ec23589
Sirok Dávid <sdavid8506@gmail.com>
2021-07-25 14:44:29 +0200

    Vizsgaremek

--- vizsgaremek/tc01_registration_test.py
+++ vizsgaremek/tc01_registration_test.py
@@ -6,7 +6,7 @@ def test_registration():
     from webdriver_manager.chrome import ChromeDriverManager
 
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
--- vizsgaremek/tc06_save_data_test.py
+++ vizsgaremek/tc06_save_data_test.py
@@ -7,7 +7,7 @@ def test_save_data():
     import time
 
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
