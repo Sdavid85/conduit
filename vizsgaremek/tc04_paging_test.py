93491a109e9f1d89eb86429242b88b4acb10de6e
Sirok Dávid <sdavid8506@gmail.com>
2021-08-10 20:28:56 +0200

    Updated

--- vizsgaremek/tc04_paging_test.py
+++ vizsgaremek/tc04_paging_test.py
@@ -13,11 +13,14 @@ def test_paging():
 
     try:
         driver.get("http://localhost:1667/#/")
-        # Cookie accept:
-        driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]').click()
 
-        # Activate Sign in input field:
-        driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
+        # Accept cookies
+        accept_btn = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div')
+        accept_btn.click()
+
+        # Login
+        login = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a')
+        login.click()
 
         time.sleep(3)
 
@@ -32,25 +35,24 @@ def test_paging():
 
         sign_in("milvus@example.com", "Abcd123$")
 
-        time.sleep(3)
-        feed_title_list = []
+        time.sleep(2)
+
+        article_title_list = []
         page_count = 1
 
         while True:
             time.sleep(2)
-            feed_titles = driver.find_elements_by_xpath('//*[@id="app"]//a/h1')
-            for feed in feed_titles:
-                feed_title_list.append(feed.text)
+            article_titles = driver.find_elements_by_xpath('//*[@id="app"]//a/h1')
+            for article in article_titles:
+                article_title_list.append(article.text)
             try:
                 page_count += 1
                 driver.find_element_by_link_text(str(page_count)).click()
-            # driver.find_element_by_xpath('//*[@id="app"]//nav/ul/li[%]/a')
             except NoSuchElementException:
-                # Stop loop if no more page available
                 break
-        print("The title-list of Global feeds: ", feed_title_list)
-        print("Number of Global feeds: ", len(feed_title_list))
+        print("The title-list of Global feeds: ", article_title_list)
+        print("Number of Global feeds: ", len(article_title_list))
 
     finally:
         pass
-        # driver.close()
+    # driver.close()
